58c9908bfd3f6e01fe858f33d3727b02b96ad6a1
Efrain Pinto <efrapp@gmail.com>
2019-12-30 10:19:05 -0500

    Use https for the Open Weather url in the weather service

--- src/js/weather_service.js
+++ src/js/weather_service.js
@@ -5,7 +5,7 @@ const DEFAULT_UNITS = 'metric';
 let units = DEFAULT_UNITS;
 
 WeatherService.prototype.getInfo = async function getInfo(city) {
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${API_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${API_KEY}`;
 
   const response = await fetch(url, { mode: 'cors' });
   const info = await response.json();
